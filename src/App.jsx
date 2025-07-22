786af782de0e63f9f0244fcc030a30c60429a967
Moustapha <amroune.moustapha@gmail.com>
2025-07-22 15:41:25 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Markdown from "./components/markdown"
+
+function App() {
+ 
+
+  return (
+    <>
+      <Markdown/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Markdown.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const markdown = () => {
+  return (
+    <div>
+      <textarea  id="editor" ></textarea>
+      <div id="preview" ></div>
+    </div>
+  )
+}
+
+export default markdown
