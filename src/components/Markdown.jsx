ed09d451980118a16b07cbf1efe192766638018d
Moustapha <amroune.moustapha@gmail.com>
2025-07-23 16:55:40 +0200

    stylization previewer and editor

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -3,11 +3,15 @@ import { marked } from 'marked'
 import DOMPurify from 'dompurify';
 import { useState } from 'react';
 
+// define the markdown previewer component
 const Markdown = () => {
+  // configure marked options for GitHub-flavored markdown and line breaks
     marked.setOptions({
   breaks: true,
-  gfm: true
+  gfm: true,
 });
+
+// Markdown content
     const templateMarkdown = `
     
 # Welcome to my React Markdown Previewer!
@@ -37,10 +41,12 @@ There's also [links](https://www.freecodecamp.org), and
 
 And if you want to get really crazy, even tables:
 
+
 Wild Header | Crazy Header | Another Header?
 ------------ | ------------- | -------------
 Your content can | be here, and it | can be here....
 And here. | Okay. | I think we get it.
+
 
 - And of course there are lists.
   - Some are bulleted.
@@ -52,17 +58,30 @@ And here. | Okay. | I think we get it.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
 
+
 ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
 
 
     `
+    // Initialize state with default Markdown content and provide an update function
     const [markdown, setMarkdown] = useState(templateMarkdown);
 
+// Convert Markdown into safe HTML to prevent injection attacks
 const cleanHTML = DOMPurify.sanitize(marked(markdown));
   return (
     <div>
+      <div id='editor-block'>
+      <h2 className='section-header'>Editor <i className="fa fa-arrows-alt icon-right"></i>
+ </h2>
       <textarea  id="editor" value={markdown}  onChange={(e) => setMarkdown(e.target.value)} />
+    </div>
+
+      <div id="preview-block">
+        <h2  className='section-header'>Previewer  <i className="fa fa-arrows-alt icon-right"></i>
+  </h2>
+  {/* Render sanitized HTML from markdown */}
       <div id="preview"    dangerouslySetInnerHTML={{ __html: cleanHTML }} ></div>
+      </div>
     </div>
   )
 }
